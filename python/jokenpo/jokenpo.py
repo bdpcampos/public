e478c93d09579e995e55743bece0a7860e0ab277
bdpcampos <bdpc2019@gmail.com>
2020-05-06 21:37:33 -0300

    Made a .pyc of jokenpo
    It seems to run like an .exe on windows.... :D

--- python/jokenpo/jokenpo.py
+++ python/jokenpo/jokenpo.py
@@ -1,27 +1,39 @@
 import random
+import subprocess
+clear = lambda: subprocess.call('cls||clear', shell=True)
 
-print('')
-print('{:=^40}'.format(' Jokenpô '))
-print('')
+r = ''
 
-jokenpo = ['Pedra', 'Papel', 'Tesoura']
+while r != 0:
+    print('')
+    print('{:=^40}'.format(' Jokenpô '))
+    print('')
 
-print('Escolha:\n1. Pedra\n2. Papel\n3. Tesoura')
+    jokenpo = ['Pedra', 'Papel', 'Tesoura']
 
-escolha = 0
+    print('Escolha:\n1. Pedra\n2. Papel\n3. Tesoura')
 
-while escolha not in [1, 2, 3]:
-    escolha = int(input())
+    escolha = 0
 
-escolha_pc = random.randint(1,3)
+    while escolha not in [1, 2, 3]:
+        escolha = int(input())
 
-print('Você escolheu {} e o computador escolheu {}'.format(jokenpo[escolha - 1], jokenpo[escolha_pc - 1]))
+    escolha_pc = random.randint(1,3)
 
-if escolha_pc == 1 and escolha == 3 or escolha_pc == 2 and escolha == 1 or escolha_pc == 3 and escolha == 2:
-    print('O computador venceu!')
+    print('Você escolheu {} e o computador escolheu {}'.format(jokenpo[escolha - 1], jokenpo[escolha_pc - 1]))
 
-elif escolha == escolha_pc:
-    print('Deu empate!')
+    if escolha_pc == 1 and escolha == 3 or escolha_pc == 2 and escolha == 1 or escolha_pc == 3 and escolha == 2:
+        print('O computador venceu!')
 
-else:
-    print('Você venceu!!!')
+    elif escolha == escolha_pc:
+        print('Deu empate!')
+
+    else:
+        print('Você venceu!!!')
+
+    print('')
+    r = int(input('Pressione 0 para sair ou 1 para continuar'))
+    print('')
+
+    clear()
+
