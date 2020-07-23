254d72e1114f62ba994d43db80b940216e34c1db
Bruno Campos <bdpc2019@gmail.com>
2020-07-22 22:35:01 -0300

    Curso de JS

---
+++ courses/javascript/script.js
@@ -0,0 +1,11 @@
+window.document.write(document.charset)
+
+//Selecionando por Marca
+
+/*
+Só funcionou colocando direto no html, não funcionou colocando em arquivo separado.
+
+var p1 = window.document.getElementsByTagName('p')[0]
+window.document.write(p1.innerText)
+
+*/
