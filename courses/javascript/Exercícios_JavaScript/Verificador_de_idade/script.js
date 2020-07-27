242e0f7fc4f25d08354a36fee545ad31785ea7b0
Bruno Campos <bdpc2019@gmail.com>
2020-07-27 14:45:41 -0300

    JS course

--- courses/javascript/Exercícios_JavaScript/Verificador_de_idade/script.js
+++ courses/javascript/Exercícios_JavaScript/Verificador_de_idade/script.js
@@ -23,24 +23,30 @@ function verificar(){
     //Modificação da div no HTML
     imagem.innerHTML = `<p>Detectamos ${sexo} com ${idade} anos.</p>`
 
-    if (idade < 0 || sexo === null) {
-        imagem.innerHTML = '<p>[ERRO] Selecione um ano válido e um sexo!</p>'
-    } else if (idade < 8 && sexo === 'Homem') {
-        imagem.innerHTML += `<img src="_images/foto-bebe-m.png" alt="">`
-    } else if (idade < 18 && sexo === 'Homem') {
-        imagem.innerHTML += `<img src="_images/foto-jovem-m.png" alt="">`
-    } else if (idade < 65 && sexo === 'Homem') {
-        imagem.innerHTML += `<img src="_images/foto-adulto-m.png" alt="">`  
+    if (idade < 0 || Object.is(NaN, ano)) {
+        imagem.innerHTML = '<p>[ERRO] Selecione um ano válido!</p>'
+    } else if (sexo === null){
+        imagem.innerHTML = '<p>[ERRO] Selecione um sexo!</p>'
     } else if (sexo === 'Homem') {
+        if (idade < 8) {
+        imagem.innerHTML += `<img src="_images/foto-bebe-m.png" alt="">`
+        } else if (idade < 18) {
+        imagem.innerHTML += `<img src="_images/foto-jovem-m.png" alt="">`
+        } else if (idade < 65) {
+        imagem.innerHTML += `<img src="_images/foto-adulto-m.png" alt="">`  
+        } else {
         imagem.innerHTML += `<img src="_images/foto-idoso-m.png" alt="">`
-    } else if (idade < 8 && sexo === 'Mulher') {
-        imagem.innerHTML += `<img src="_images/foto-bebe-f.png" alt="">`
-    } else if (idade < 18 && sexo === 'Mulher') {
-        imagem.innerHTML += `<img src="_images/foto-jovem-f.png" alt="">`
-    } else if (idade < 65 && sexo === 'Mulher') {
-        imagem.innerHTML += `<img src="_images/foto-adulto-f.png" alt="">`  
+        }
     } else if (sexo === 'Mulher') {
+        if (idade < 8) {
+        imagem.innerHTML += `<img src="_images/foto-bebe-f.png" alt="">`
+        } else if (idade < 18) {
+        imagem.innerHTML += `<img src="_images/foto-jovem-f.png" alt="">`
+        } else if (idade < 65) {
+        imagem.innerHTML += `<img src="_images/foto-adulto-f.png" alt="">`  
+        } else {
         imagem.innerHTML += `<img src="_images/foto-idoso-f.png" alt="">`
     } 
+    }
   
 }
