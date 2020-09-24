da124cc02deaed3aa2dd5cd9cec814bf48d14b64
bdpcampos <bdpc2019@gmail.com>
2020-09-23 23:44:41 -0300

    JS course and trainning

---
+++ courses/javascript/Rocketseat/Curso JS/Aula3 - App/app.js
@@ -0,0 +1,75 @@
+
+//Global variables declaration
+
+const listElement = document.querySelector('#app ul');
+const inputElement = document.querySelector('#app input');
+const buttonElement = document.querySelector('#app button');
+let toDos = [];
+
+
+//Functions declaration
+
+function createToDoElement(element) {
+    const line = document.createElement('li');
+    const lineText = document.createTextNode(element);
+    const linkElement = document.createElement('a');
+    const linkText = document.createTextNode('Excluir');
+
+    toDos.push(element);
+
+    saveToDosLocal();
+
+    const elementPositionOnArray = toDos.indexOf(element);
+
+    linkElement.setAttribute('onclick', `deleteToDoElement('${elementPositionOnArray}')`)
+    linkElement.setAttribute('href','#')
+    linkElement.appendChild(linkText);
+
+    line.setAttribute('id',`pos${elementPositionOnArray}`)
+    line.appendChild(lineText);
+    line.appendChild(linkElement);
+
+    listElement.appendChild(line);
+}
+
+
+function deleteToDoElement(pos) {
+    toDos.splice(pos, 1);
+    saveToDosLocal();
+
+    const element = document.querySelector(`#pos${pos}`)
+    element.parentElement.removeChild(element);
+}
+
+
+function saveToDosLocal() {
+    localStorage.toDos = JSON.stringify(toDos);
+}
+
+
+function newToDo() {
+    inputElement.value !== "" && !toDos.includes(inputElement.value)? createToDoElement(inputElement.value) : alert('Adicione uma tarefa válida!');
+
+    inputElement.value = null;
+}
+
+
+function renderToDos() {
+    if(localStorage.toDos){
+    const initToDos = JSON.parse(localStorage.toDos);
+
+    for(let element of initToDos) {
+        createToDoElement(element);
+    }
+    }
+}
+
+
+//Code
+
+renderToDos();
+
+buttonElement.addEventListener('click', newToDo, false);
+
+
+
